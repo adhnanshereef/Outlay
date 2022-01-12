55517b94de6138855176f956daafc4f1c3146f0e
Adhnan Shereef T <93500202+AdhnanShereef@users.noreply.github.com>
2022-01-12 13:05:32 +0530

    Settings UI Done
    Settings UI is completely done but it's not functioned

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -41,66 +41,68 @@ class _HomeState extends State<Home> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: PreferredSize(
-          preferredSize: const Size(double.infinity, 77),
-          child: Container(
-            // height: 100,
-            decoration: BoxDecoration(
-              // border: Border.all(color: Colors.white),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0.0, 1.0), //(x,y)
-                  blurRadius: 5.0,
-                ),
-              ],
+        appBar: _currentSelectedIndex != 2
+            ? PreferredSize(
+                preferredSize: const Size(double.infinity, 77),
+                child: Container(
+                  // height: 100,
+                  decoration: BoxDecoration(
+                    // border: Border.all(color: Colors.white),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 1.0), //(x,y)
+                        blurRadius: 5.0,
+                      ),
+                    ],
 
-              gradient: const LinearGradient(
-                stops: [
-                  0.10,
-                  0.90,
-                ],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  Color(0xFF09102a),
-                  Color(0xFF003157),
-                ],
-              ),
-              color: Colors.deepPurple.shade300,
-              borderRadius:
-                  const BorderRadius.vertical(bottom: Radius.circular(25.0)),
-            ),
-            child: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    gradient: const LinearGradient(
+                      stops: [
+                        0.10,
+                        0.90,
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xFF09102a),
+                        Color(0xFF003157),
+                      ],
+                    ),
+                    color: Colors.deepPurple.shade300,
+                    borderRadius: const BorderRadius.vertical(
+                        bottom: Radius.circular(25.0)),
+                  ),
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          'Outlay',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Outlay',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Hi' $userName",
+                                style: const TextStyle(color: Colors.white),
+                              )
+                            ],
                           ),
                         ),
-                        Text(
-                          "Hi' $userName",
-                          style: const TextStyle(color: Colors.white),
-                        )
                       ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
+                ),
+              )
+            : null,
         body: SafeArea(
           child: tabs[_currentSelectedIndex],
         ),
--- lib/screens/home/settings.dart
+++ lib/screens/home/settings.dart
@@ -1,19 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:outlay/main.dart';
+import 'package:outlay/screens/home/home.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Outlay Settings',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 65,
-          letterSpacing: 5,
-          fontWeight: FontWeight.bold,
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ListView(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 75,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              border: Border.all(
+                color: Colors.white,
+              ),
+            ),
+            child: Center(
+              child: ListTile(
+                title: Text(
+                  userName,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                ),
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.account_circle_sharp,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+              child: const Center(
+                child: ListTile(
+                  title: Text(
+                    'Export',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.arrow_upward,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+              child: const Center(
+                child: ListTile(
+                  title: Text(
+                    'About',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+              child: const Center(
+                child: ListTile(
+                  title: Text(
+                    'Help',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.help,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              height: 75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+              child: const Center(
+                child: ListTile(
+                  title: Text(
+                    'Report bugs or new suggestions',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.bug_report,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
